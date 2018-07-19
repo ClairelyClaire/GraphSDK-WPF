83c4e0b3581d2fff9bfae106e21bb152295522c8
Claire Davis (Strodtbeck) <clstrodt@microsoft.com>
2018-07-19 09:51:55 -0700

    Initial commit

--- GraphSDK WPF/App.xaml.cs
+++ GraphSDK WPF/App.xaml.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.Identity.Client;
 using System.Windows;
 
 namespace GraphSDK_WPF
@@ -13,5 +8,12 @@ namespace GraphSDK_WPF
     /// </summary>
     public partial class App : Application
     {
+        // portal.azure.com
+        private static string AzureAppId = "9cc033c6-6227-4c3a-992e-977169a77447";
+        public static PublicClientApplication AzureApp = new PublicClientApplication(AzureAppId);
+
+        // apps.dev.microsoft.com
+        private static string DevAppId = "dea2b33e-dccb-4f90-92e1-0594fbc93fa8";
+        public static PublicClientApplication DevApp = new PublicClientApplication(DevAppId);
     }
 }
--- GraphSDK WPF/MainWindow.xaml.cs
+++ GraphSDK WPF/MainWindow.xaml.cs
@@ -1,17 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Microsoft.Identity.Client;
+using System;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace GraphSDK_WPF
 {
@@ -20,9 +10,95 @@ namespace GraphSDK_WPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        string[] _scopes = new string[] { "User.Read" };
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private async void cmdAzureLogin_Click(object sender, RoutedEventArgs e)
+        {
+            // results of auth token request
+            AuthenticationResult authResult = null;
+
+            try
+            {
+                // MSAL points to the AAD/MSA v2 endpoint by default
+                // nothing else is needed here, I think
+
+                // acquire silently first - this will refresh an existing token or sign in the current user (?)
+                authResult = await App.AzureApp.AcquireTokenSilentAsync(_scopes, App.AzureApp.Users.FirstOrDefault());
+            }
+            catch (MsalUiRequiredException ex)
+            {
+                // if an exception occurs, AcquireTokenAsync is required (interactive login)
+                txtResult.Text = $"MsalUiRequiredException: {ex.Message}";
+
+                // try an interactive login
+                try
+                {
+                    authResult = await App.AzureApp.AcquireTokenAsync(_scopes);
+                }
+                catch (MsalException msalex)
+                {
+                    // something terrible happened
+                    txtResult.Text = $"Error Acquiring Token:{System.Environment.NewLine}{msalex}";
+                }
+            }
+            catch (Exception ex)
+            {
+                // something terrible happened
+                txtResult.Text = $"Error Acquiring Token Silently:{System.Environment.NewLine}{ex}";
+            }
+
+            // now you can try to do something with the returned auth stuffs.
+            if (authResult != null)
+            {
+                // do a thing
+            }
+        }
+
+        private async void cmdDevLogin_Click(object sender, RoutedEventArgs e)
+        {
+            // results of auth token request
+            AuthenticationResult authResult = null;
+
+            try
+            {
+                // MSAL points to the AAD/MSA v2 endpoint by default
+                // nothing else is needed here, I think
+
+                // acquire silently first - this will refresh an existing token or sign in the current user (?)
+                authResult = await App.DevApp.AcquireTokenSilentAsync(_scopes, App.DevApp.Users.FirstOrDefault());
+            }
+            catch (MsalUiRequiredException ex)
+            {
+                // if an exception occurs, AcquireTokenAsync is required (interactive login)
+               txtResult.Text = $"MsalUiRequiredException: {ex.Message}";
+
+                // try an interactive login
+                try
+                {
+                    authResult = await App.DevApp.AcquireTokenAsync(_scopes);
+                }
+                catch (MsalException msalex)
+                {
+                    // something terrible happened
+                    txtResult.Text = $"Error Acquiring Token:{System.Environment.NewLine}{msalex}";
+                }
+            }
+            catch (Exception ex)
+            {
+                // something terrible happened
+                txtResult.Text = $"Error Acquiring Token Silently:{System.Environment.NewLine}{ex}";
+            }
+
+            // now you can try to do something with the returned auth stuffs.
+            if (authResult != null)
+            {
+                // do a thing
+            }
         }
     }
 }
